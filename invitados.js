6772e443fc6a5fdda6557de2a0a74e74f87a212d
hinojosalopezmonica-gif <hinojosalopezmonica@gmail.com>
2026-07-22 13:23:19 -0700

    Add files via upload

---
+++ invitados.js
@@ -0,0 +1,258 @@
+window.INVITADOS = {
+  "001": {
+    "nombre": "Beatriz Avalos",
+    "lugares": 2
+  },
+  "002": {
+    "nombre": "Aleyda Avalos",
+    "lugares": 1
+  },
+  "003": {
+    "nombre": "Armando y Claudia",
+    "lugares": 2
+  },
+  "004": {
+    "nombre": "Familia Cervantes Gonzalez",
+    "lugares": 2
+  },
+  "005": {
+    "nombre": "Natalia Cervantes",
+    "lugares": 2
+  },
+  "006": {
+    "nombre": "Regina y Diana",
+    "lugares": 3
+  },
+  "007": {
+    "nombre": "Abraham Sanchez",
+    "lugares": 2
+  },
+  "008": {
+    "nombre": "Alma García",
+    "lugares": 2
+  },
+  "009": {
+    "nombre": "Lourdes Valenzuela",
+    "lugares": 1
+  },
+  "010": {
+    "nombre": "Roberto Quinteromarmol",
+    "lugares": 4
+  },
+  "011": {
+    "nombre": "Enrique Avalos",
+    "lugares": 2
+  },
+  "012": {
+    "nombre": "Edgardo y Elizabeth",
+    "lugares": 2
+  },
+  "013": {
+    "nombre": "Enrique Avalos Rangel",
+    "lugares": 1
+  },
+  "014": {
+    "nombre": "Ramses Avalos Rangel",
+    "lugares": 1
+  },
+  "015": {
+    "nombre": "Yolanda Solis",
+    "lugares": 1
+  },
+  "016": {
+    "nombre": "Guillermo Avalos",
+    "lugares": 1
+  },
+  "017": {
+    "nombre": "Heriberto Avalos",
+    "lugares": 2
+  },
+  "018": {
+    "nombre": "Yahir Avalos",
+    "lugares": 2
+  },
+  "019": {
+    "nombre": "Otto Avalos",
+    "lugares": 2
+  },
+  "020": {
+    "nombre": "Alberto Mascareño",
+    "lugares": 1
+  },
+  "021": {
+    "nombre": "Verónica Avalos",
+    "lugares": 1
+  },
+  "022": {
+    "nombre": "Familia Jaramillo Avalos",
+    "lugares": 3
+  },
+  "023": {
+    "nombre": "Javier Nevarez",
+    "lugares": 2
+  },
+  "024": {
+    "nombre": "Rodrigo Nevarez",
+    "lugares": 2
+  },
+  "025": {
+    "nombre": "Sergio Jaramillo",
+    "lugares": 2
+  },
+  "026": {
+    "nombre": "Omar Avalos",
+    "lugares": 3
+  },
+  "027": {
+    "nombre": "Osiris Avalos",
+    "lugares": 1
+  },
+  "028": {
+    "nombre": "Omar Avalos Lomelí",
+    "lugares": 2
+  },
+  "029": {
+    "nombre": "Lorena Avalos",
+    "lugares": 3
+  },
+  "030": {
+    "nombre": "Claudia López",
+    "lugares": 2
+  },
+  "031": {
+    "nombre": "Mónica López",
+    "lugares": 2
+  },
+  "032": {
+    "nombre": "Juan Hinojosa Botello",
+    "lugares": 3
+  },
+  "033": {
+    "nombre": "Faviola Hinojosa",
+    "lugares": 1
+  },
+  "034": {
+    "nombre": "Juan Hinojosa",
+    "lugares": 2
+  },
+  "035": {
+    "nombre": "Julia Hinojosa",
+    "lugares": 4
+  },
+  "036": {
+    "nombre": "Maria Elena Hinojosa",
+    "lugares": 2
+  },
+  "037": {
+    "nombre": "Lizbeth Hinojosa",
+    "lugares": 2
+  },
+  "038": {
+    "nombre": "Marlen Hinojosa",
+    "lugares": 2
+  },
+  "039": {
+    "nombre": "Erica Hinojosa",
+    "lugares": 5
+  },
+  "040": {
+    "nombre": "Mauricio Hinojosa",
+    "lugares": 2
+  },
+  "041": {
+    "nombre": "Guadalupe Hinojosa",
+    "lugares": 3
+  },
+  "042": {
+    "nombre": "Francisco Sanchez",
+    "lugares": 2
+  },
+  "043": {
+    "nombre": "Carlos Hinojosa",
+    "lugares": 2
+  },
+  "044": {
+    "nombre": "Mauricio Hinojosa",
+    "lugares": 2
+  },
+  "045": {
+    "nombre": "Gloria y Gilberto",
+    "lugares": 2
+  },
+  "046": {
+    "nombre": "Elena López",
+    "lugares": 2
+  },
+  "047": {
+    "nombre": "Angela y Tilo",
+    "lugares": 2
+  },
+  "048": {
+    "nombre": "Karen y Santiago",
+    "lugares": 2
+  },
+  "049": {
+    "nombre": "Javier Hinojosa",
+    "lugares": 2
+  },
+  "050": {
+    "nombre": "Reyes y Rebeca",
+    "lugares": 2
+  },
+  "051": {
+    "nombre": "Citlali Ochoa",
+    "lugares": 1
+  },
+  "052": {
+    "nombre": "Javier López",
+    "lugares": 2
+  },
+  "053": {
+    "nombre": "Andrea López",
+    "lugares": 2
+  },
+  "054": {
+    "nombre": "Gilberto López",
+    "lugares": 2
+  },
+  "055": {
+    "nombre": "Gloria y Marco",
+    "lugares": 2
+  },
+  "056": {
+    "nombre": "Aleyda y Ailin",
+    "lugares": 2
+  },
+  "057": {
+    "nombre": "Catherine y Jesus",
+    "lugares": 3
+  },
+  "058": {
+    "nombre": "Juan Ramon Ernesto",
+    "lugares": 1
+  },
+  "059": {
+    "nombre": "Cristina Fierro",
+    "lugares": 1
+  },
+  "060": {
+    "nombre": "Dayan Garcia",
+    "lugares": 2
+  },
+  "061": {
+    "nombre": "Karla Monreal",
+    "lugares": 2
+  },
+  "062": {
+    "nombre": "Berenice Martinez",
+    "lugares": 2
+  },
+  "063": {
+    "nombre": "Angélica Luna",
+    "lugares": 2
+  },
+  "064": {
+    "nombre": "Ávalos Hinojosa",
+    "lugares": 2
+  }
+};
