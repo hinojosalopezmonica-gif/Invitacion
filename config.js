e23ccdbec860d8e6bb6aa211f2f1a8f87c054361
hinojosalopezmonica-gif <hinojosalopezmonica@gmail.com>
2026-07-22 13:18:58 -0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.CONFIG = {
-  // Pega aquí la URL de implementación de Google Apps Script cuando esté lista.
-  appsScriptUrl: "",
-  mapsUrl: "https://maps.app.goo.gl/uwyYwAMMSCUEpFF99?g_st=ac"
-};
