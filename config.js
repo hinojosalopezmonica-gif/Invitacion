5104fa3d33da790cabca30c3fc228852adbe5b93
hinojosalopezmonica-gif <hinojosalopezmonica@gmail.com>
2026-07-22 20:15:01 -0700

    Update appsScriptUrl to production URL

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.CONFIG = {
-  // Pega aquí la URL de implementación de Google Apps Script cuando esté lista.
-  appsScriptUrl: "",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbylI3a6EGHQ2vGeUvBmPuWQxfWphucu0jhK2o2_k9lItv-zq5nUVsnaQ46tNnHnEuPF/exec",
   mapsUrl: "https://maps.app.goo.gl/uwyYwAMMSCUEpFF99?g_st=ac"
 };
